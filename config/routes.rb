cdd514a0d6707a0a641d16a027b2c635e6fce2ad
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-10-03 15:18:46 +0200

    posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  mount Wellspring::Engine, at: '/admin'
+  mount Wellspring::Engine, at: "/admin"
+
+  get '/:slug', to: 'blog_posts#show'
+  root to: 'blog_posts#index'
 end
